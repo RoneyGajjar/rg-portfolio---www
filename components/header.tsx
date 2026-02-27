eb5ecd84bca7010b816c4028aafff9aa2fe8b9ff
Roney Gajjar <roneygajjar121@gmail.com>
2026-02-28 01:41:24 +0530

    feat: update header and work components with new images, descriptions, and tech stack details

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <header className="flex  justify-between max-w-7xl mx-auto p-4">
             <div className="flex flex-row text-base">
-                <img src="profile-photo.jpg" alt="" className="h-10 w-10 rounded-full" />
+                <img src="profile-photo.png" alt="" className="h-10 w-10 rounded-full" />
                 <div className="flex flex-col ml-3">
                     <span className="font-semibold">RG</span>
                     <span className="text-sm">Presently at Honey Hexa</span>
--- components/work.tsx
+++ components/work.tsx
@@ -4,28 +4,36 @@ import Link from "next/link";
 const workData = [
     {
         id: 1,
-        img: "profile-photo.jpg",
+        img: "punit-valves.png",
         title: "Punit Valves",
-        description: "Organizing complex technical specifications and product catalogs into a user-friendly interface for procurement professionals."
+        description: "Organizing complex technical specifications and product catalogs into a user-friendly interface for procurement professionals.",
+        techStack: "React, JavaScript, Tailwind CSS.",
         link: "https://punitvalves.com/"
+
     },
     {
         id: 2,
-        img: "profile-photo.jpg",
+        img: "rk-cinematics.png",
         title: "RK-Cinematics",
-        description: "Description"
+        description: "Description",
+        techStack: "React, JavaScript, Tailwind CSS.",
+        link: "https://cinematics-www-git-rk-cinematics-roney-gajjars-projects.vercel.app/"
     },
     {
         id: 3,
-        img: "profile-photo.jpg",
+        img: "harshid-engravers.png",
         title: "Harshid Engravers",
-        description: "Transitioning a traditional engraving MSME from manual, error-prone paper logging to a real-time digital tracking system."
+        description: "Transitioning a traditional engraving MSME from manual, error-prone paper logging to a real-time digital tracking system.",
+        techStack: "React, JavaScript, Tailwind CSS.",
+        link: "#"
     },
     {
         id: 4,
-        img: "profile-photo.jpg",
+        img: "kuber-creations.png",
         title: "Kuber Creations",
-        description: "Building a high-performance showcase for handicrafts that could load quickly for a global audience."
+        description: "Building a high-performance showcase for handicrafts that could load quickly for a global audience.",
+        techStack: "React, JavaScript, Tailwind CSS.",
+        link: "https://kubercreations.com/"
     }
 ]
 
@@ -38,6 +46,7 @@ const Work = () => {
                     title={item.title}
                     description={item.description}
                     img={item.img}
+                    techStack={item.techStack}
                     link={item.link}
                 />
             ))
@@ -51,7 +60,7 @@ export default Work;
 
 
 
-const WorkCard = ({ title = "Work Title", description = "Work Description", img = "Image", link = "#" }) => (
+const WorkCard = ({ title = "Work Title", description = "Work Description", img = "Image", link = "#", techStack = "Tech Stack" }) => (
     <div className="flex flex-col items-center justify-center gap-10 mt-15 bg-slate-100 p-2 rounded-xl mx-auto max-w-5xl">
         <div className="w-full">
             <img src={img} alt="Work Image" className="w-full h-[500] object-cover  rounded-xl border border-8 border-white shadow-md" />
@@ -60,14 +69,15 @@ const WorkCard = ({ title = "Work Title", description = "Work Description", img 
             <div className="flex flex-row justify-between w-235">
                 <div>
                     <span className="text-xl text-slate-800 font-semibold">{title}</span>
-                    <Link href={link}></Link>
+                    <span className="ml-5"><Link href={link}>View Project</Link></span>
                 </div>
                 <div>
                     <Button />
                 </div>
             </div>
-            <div className="text-2xl max-w-2xl pb-5">
+            <div className="text-xl max-w-2xl pb-5">
                 <h2 className="text-slate-700">{description}</h2>
+                <h3 className="text-slate-500 mt-2">{techStack}</h3>
             </div>
         </div>
     </div>
